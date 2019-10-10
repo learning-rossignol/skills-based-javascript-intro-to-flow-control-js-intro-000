9b6ec6cab95df046f2a0454f6f9325410746e381
Jean-François Rompré <jrompre@gmail.com>
2019-10-10 21:40:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,11 +12,18 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if ( age <= 19 &&  13 <= age)
+      return "You are a teenager!"
+  else if (age <= 12)
+      return "You are a kid"
+  else if (age > 19)
+      return "You are a grownup"
 }
 
 function ternaryTeenager(age) {
-
+   return "You are "  
+        + (( age <= 19 &&  13 <= age) ?? "a" : "not a") 
+        + " teenager"
 }
 
 function switchAge(age) {
